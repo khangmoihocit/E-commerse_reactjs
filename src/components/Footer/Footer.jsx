99c4ea296a0fe06942a245d479794fb702a36dde
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-07-17 22:06:21 +0700

    modified:   src/components/Footer/Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
+        <>
         <div>
             footer
         </div>
+        <div>
+            a
+        </div>
+</>
     );
 };
 
