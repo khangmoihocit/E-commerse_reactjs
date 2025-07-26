8c8887d6e56e68dd0494b349dd735818038e9f87
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-07-26 17:03:58 +0700

    feat: create initial structure and JSX for SaleHomePage

--- src/components/SaleHomePage/SaleHomePage.jsx
+++ src/components/SaleHomePage/SaleHomePage.jsx
@@ -3,18 +3,31 @@ import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
 
 const SaleHomePage = () => {
-    const {container} = styles;
+    const { container, title, desc, boxBtn, boxImage, boxContent } = styles;
     return (
         <div className={container}>
-            <div>
-                <img src="https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image_1.jpeg" alt="" />
+            <div className={boxImage}>
+                <img
+                    src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image_1.jpeg'
+                    alt=''
+                />
             </div>
-            <div>
-                <h2>Sale Of The Year</h2>
-                <p>Libero sed faucibus facilisis fermentum. Est nibh sed massa sodales.</p>
-                <Button content={'Read more'} isPrimary={false}/>
+            <div className={boxContent}>
+                <h2 className={title}>Sale Of The Year</h2>
+                <p className={desc}>
+                    Libero sed faucibus facilisis fermentum. Est nibh sed massa
+                    sodales.
+                </p>
+                <div className={boxBtn}>
+                    <Button content={'Read more'} isPrimary={false} />
+                </div>
             </div>
-            <div><img src="https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image_2.jpeg" alt="" /></div>
+            <div className={boxImage}>
+                <img
+                    src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image_2.jpeg'
+                    alt=''
+                />
+            </div>
         </div>
     );
 };
